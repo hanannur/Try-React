5d1c0adaea5d6adfa879377ce14af077dec4d619
Hanan-Nuru <hanannuru65@gmail.com>
2026-03-03 15:36:09 +0300

    edit the input button

--- react_basics/src/components/InputArea.jsx
+++ react_basics/src/components/InputArea.jsx
@@ -9,7 +9,7 @@ const InputArea= ({onAdd}) => {
   }
   return (
     <div className="item-center flex items-center mb-4">
-        <input  type="text" placeholder="Add your task" onChange={(e)=>setInputText(e.target.value)} className=" bg-whiteshadow-md item-center border border-gray-300 w-1/2 rounded-lg p-2 gap-3px" />
+        <input  type="text" placeholder="Add your task" value={inputText} onChange={(e)=>setInputText(e.target.value)} className=" bg-whiteshadow-md item-center border border-gray-300 w-1/2 rounded-lg p-2 gap-3px" />
         <button onClick={handleClick} className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2m ml-2">Add</button>
     </div>
   )
